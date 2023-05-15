646f62f72bae099639b9beb367f1eec8965763ec
Sleepingoff <just_jh30@naver.com>
2023-05-15 09:36:58 +0900

    message for connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("message", (message) =>{
+    console.log(`Just got this: ${message.data} from the Server at ${message.timeStamp}`);
+});
+
+socket.addEventListener("open", ()=>{
+    console.log("Connected to Browser");
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected: Connection closed by Server");
+});
+
+socket.addEventListener("error", () => {
+    console.log("Error");
+});
+
+setTimeout(()=>{
+    socket.send("hello from the browser!");
+},10000);
